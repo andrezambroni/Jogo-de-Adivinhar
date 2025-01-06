c0f1aa0d057094039c673786b8f5d841dfc4593e
andrezambroni <andrezambroni@gmail.com>
2025-01-06 12:59:19 -0300

    1

---
+++ src/utils/word.ts
@@ -0,0 +1,13 @@
+export type Challenge = {
+  id: number
+  word: string
+  tip: string
+}
+
+export const WORDS: Challenge[] = [
+  { id: 1, word: "CSS", tip: "Linguagem de estilos" },
+  { id: 2, word: "REACT", tip: "Biblioteca de componentes" },
+  { id: 3, word: "HTML", tip: "Linguagem de marcação" },
+  { id: 4, word: "JAVASCRIPT", tip: "Linguagem de programação" },
+  { id: 5, word: "TYPESCRIPT", tip: "Para adicionar tipagem ao JavaScript" },
+]
