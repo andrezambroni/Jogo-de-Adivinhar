9d89a9343c1291606e6cf002eaf70f13e35ec838
andrezambroni <andrezambroni@gmail.com>
2025-01-07 16:32:32 -0300

    feat: add variacoes de tamanho e cor da letra

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,10 +1,15 @@
-
 import styles from "./styles.module.css"
 
-export function LettersUsed(){
-    return (
-      <div className={styles.lettersUsed}>
-        <h5>Letras utilizadas</h5>
+import { Letter } from "../Letter"
+
+export function LettersUsed() {
+  return (
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+      <div>
+        <Letter value="x" size="small" color="correct" />
+        <Letter value="y" size="small" color="wrong" />
       </div>
-    )
+    </div>
+  )
 }
