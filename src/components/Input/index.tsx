3df7a06a24b1ca813ed68b538805c363a8bc7f33
andrezambroni <andrezambroni@gmail.com>
2025-01-07 15:13:38 -0300

    feat: criando componente Button

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css"
 
-type Props = ComponentProps<"input">
+type Props = React.ComponentProps<"input">
 
 export function Input({...rest}: Props) {
     return <input type="text"  className={styles.input} {...rest} />
