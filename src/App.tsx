d032ee5cdd8e9694fd447428992dc3236f656bb7
andrezambroni <andrezambroni@gmail.com>
2025-01-07 14:26:52 -0300

    feat: criando componente input

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import styles from "./app.module.css"
 
 import { Tip } from "./components/Tip"
 import { Header } from "./components/Header"
-import {Letter} from "./components/Letter"
+import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
 
 export default function App() {
   function handleRestartGame() {
@@ -27,7 +28,7 @@ export default function App() {
         <h4>Palpites</h4>
 
         <div>
-          
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
