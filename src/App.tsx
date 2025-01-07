39058c7c2c8cbabb78a384cb49b163efb8a5e7d5
andrezambroni <andrezambroni@gmail.com>
2025-01-07 13:58:00 -0300

    feat: criando componente Lertter e estilizando main

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Tip } from "./components/Tip"
 import { Header } from "./components/Header"
+import {Letter} from "./components/Letter"
 
 export default function App() {
   function handleRestartGame() {
@@ -14,6 +15,20 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Linguagem de programação dinamica" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+          <Letter value="R" />
+        </div>
+
+        <h4>Palpites</h4>
+
+        <div>
+          
+        </div>
       </main>
     </div>
   )
